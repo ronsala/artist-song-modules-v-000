8fe2984532997a962b25480c254f886cf10c01f4
Ron Sala <ron.sala@gmail.com>
2018-02-06 15:10:41 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,13 +4,14 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super
     @songs = []
   end
 
